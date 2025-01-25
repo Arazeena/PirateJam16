0d7dbf2c5d2c6be56bec82a023d6303ab1e7d732
Arazeena <countess.arazeena@gmail.com>
2025-01-25 18:50:22 +0100

    Screen driven targeting

--- Assets/Scripts/Combat/Targeting/Targeter.cs
+++ Assets/Scripts/Combat/Targeting/Targeter.cs
@@ -8,6 +8,11 @@ public class Targeter : MonoBehaviour
     [SerializeField] private CinemachineTargetGroup cineTargetGroup;
     public Target CurrentTarget { get; private set; }
     private List<Target> targets = new List<Target>();
+    private Camera camera;
+
+    private void Start(){
+        camera = Camera.main;
+    }
 
     private void OnTriggerEnter(Collider other){
         
@@ -29,7 +34,26 @@ public class Targeter : MonoBehaviour
         if(targets.Count == 0)
             return false;
 
-        CurrentTarget = targets[0];
+        Target closestTarget = null;
+        float closestDistance = Mathf.Infinity;
+
+        foreach(Target target in targets){
+            Vector2 viewPos = camera.WorldToViewportPoint(target.transform.position);
+            if(viewPos.x > 1f || viewPos.x < 0f
+                || viewPos.y > 1f || viewPos.y < 0f)
+                continue;
+
+            float dist = (viewPos - new Vector2(0.5f, 0.5f)).SqrMagnitude();
+            if(dist < closestDistance){
+                closestTarget = target;
+                closestDistance = dist;
+            }
+        }  
+
+        if(closestTarget == null)
+            return false;
+
+        CurrentTarget = closestTarget;
         cineTargetGroup.AddMember(CurrentTarget.transform, 1f, 2f);
         return true;
     }
